11611a5d7ba5ccb04d55db61c7b95c11c1bcd857
jeon-maker <77326600+jeon-maker@users.noreply.github.com>
2022-03-29 01:04:32 +0900

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class test{
+    public static void main(String[] args){
+        System.out.println("Hello, world.");
+    }
+}
+/*
+* boolean : 1byte
+* char : 2byte
+* byte, short, int ,long
+* float, double
+*
+*
+* */
